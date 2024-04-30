3ada5ab6495031dc366f96885940c8943ff6b198
Aurelia H <aurelia.h@hotmail.fr>
2024-04-30 16:14:02 +0200

    Suppression de qlq commentaires

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,37 +10,17 @@ export default function HomePage() {
 
   // const links = usePortfolioContextProvider()
 
-  return <div className="containerTW"
-        // sm:max-w-4xl 
-        >
-          {/* A voir si mieux ORANGE ou BLEU ??? et ROTATE 6 ou 12 ??? */}
-          {/* inset-0 définit top, right, bottom, et left à 0. */}
-          {/* <div className="absolute inset-0 bg-gradient-to-r from-yellow-400 to-orange-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:rotate-6 sm:rounded-3xl sm:p-12 max-w-xl md:max-w-2xl lg:max-w-4xl mx-auto"></div> */}
-          {/* <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-400 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:rotate-6 sm:rounded-3xl"></div> */}
-          {/* <div className="absolute inset-0 bg-gradient-to-r from-blue-200 to-blue-300 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:rotate-6 sm:rounded-3xl"></div> */}
-          {/* <div
-            // shadow-lg 
-            className="
-                        relative 
-                        px-4
-                        py-10 
-                        bg-white
-                        sm:rounded-3xl sm:p-12 lg:p-16
-                        max-w-xl md:max-w-2xl lg:max-w-4xl
-                        mx-auto"> */}
-          {/* <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-12"> */}
-          {/* <div className="max-w-xl mx-auto"> */}
+  return (
+    <div className="containerTW">
+      <div className='boxTW'>
+        <Presentation />
+        <Skills />
+      </div>
 
-          <div
-            className='boxTW' 
-          >
-            <Presentation />
-            <Skills />
+      <Projects />
+      <Contact />
     </div>
-
-    <Projects />
-    <Contact />
-  </div>
+  )
 
       {/* More (links) */}
       {/* <div className="mt-16 flex flex-wrap justify-around gap-7">
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,97 +3,67 @@
 import React from 'react'
 import { motion } from 'framer-motion';
 
-// IMPOSSIBLE DE METTRE UN PDF !!! => Module parse failed: Unexpected character '�' (1:2) You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders
-// => j'ai essayé pendant 2h avec les forums (plein de solutions mais ne marchent pas pour moi) et copilot mais rien ne marche !!! => donc j'ai mis à la place une image en HD
-// import { Document, Page, pdfjs } from 'react-pdf';
-// import { GlobalWorkerOptions } from 'pdfjs-dist';
-// GlobalWorkerOptions.workerSrc = '/pdf.worker.js';
-// //pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
-// //pdfjsLib.GlobalWorkerOptions.workerSrc = '/pdf.worker.js';
-
-// import { Viewer } from '@react-pdf-viewer/core';
-// import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-// //  styles
-// import '@react-pdf-viewer/core/lib/styles/index.css';
-// import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-// // faire aussi pnpm install canvas
-
-
 export default function About() {
 
   const aboutMeTitle = "About\u00a0me..."
-
-  //const defaultLayoutPluginInstance = defaultLayoutPlugin();
-
-
-  // Enflish speaker
-  // Mike codeur
-  // Mes valeurs
-  return <div
-    // initial={{ opacity: 0 }}
-    // animate={{ opacity: 1 }}
-    // transition={{ duration: 1 }}
-
-    //className="flex flex-col items-center justify-center bg-gray-100 text-gray-800"
-    // min-h-screen 
-
-    //containerTW + j'écrase ces valeurs : md:gap-8 lg:gap-8
-    className="containerTW md:gap-8 lg:gap-8
-       boxTW"
-  // sm:max-w-4xl
-  >
-    <div className='flex' >
-      {aboutMeTitle.split('').map((letter, index) => (
-        <motion.span
-          key={index}
-          initial={{
-            //y: -50, 
-            opacity: 0
-          }}
-          whileInView={{ y: 0, opacity: 1 }}
-          //animate={{ y: 0, opacity: 1 }}
-          transition={{
-            duration: 0.1,
-            delay: 0.5 + index / 5
-          }}
-          className="text-3xl sm:text-4xl font-bold text-cyan-300"
-        >
-          {letter}
-        </motion.span>
-      ))}
-    </div>
-
-    <motion.p
-      initial={{ y: -50, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 1, delay: 0.5 }}
-      className="text-xl"
+  return (
+    <div
+      //containerTW + j'écrase ces valeurs : md:gap-8 lg:gap-8
+      className="containerTW md:gap-8 lg:gap-8
+                boxTW"
     >
-      Passionate web developer, I am driven by the desire to create efficient, aesthetic and user-friendly websites.
-    </motion.p>
+      <div className='flex' >
+        {aboutMeTitle.split('').map((letter, index) => (
+          <motion.span
+            key={index}
+            initial={{
+              //y: -50, 
+              opacity: 0
+            }}
+            whileInView={{ y: 0, opacity: 1 }}
+            //animate={{ y: 0, opacity: 1 }}
+            transition={{
+              duration: 0.1,
+              delay: 0.5 + index / 5
+            }}
+            className="text-3xl sm:text-4xl font-bold text-cyan-300"
+          >
+            {letter}
+          </motion.span>
+        ))}
+      </div>
 
-    <motion.p
-      initial={{ y: -50, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 1, delay: 0.8 }}
-      className="text-xl"
-    >
-      Motivated by technical challenges and human interactions, I put my expertise at the service of your projects to offer you innovative and sustainable web solutions.
-    </motion.p>
+      <motion.p
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1, delay: 0.5 }}
+        className="text-xl"
+      >
+        Passionate web developer, I am driven by the desire to create efficient, aesthetic and user-friendly websites.
+      </motion.p>
 
-    <motion.a
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.1, delay: 1 }}
-      href="/images/CV Aurélia Heymann DeveloppeurFRONT.pdf"
-      download
-      className="px-4 py-2 bg-cyan-500 text-white font-bold rounded hover:bg-cyan-600 transition-colors"
-    >
-      Download CV
-    </motion.a>
+      <motion.p
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1, delay: 0.8 }}
+        className="text-xl"
+      >
+        Motivated by technical challenges and human interactions, I put my expertise at the service of your projects to offer you innovative and sustainable web solutions.
+      </motion.p>
+
+      <motion.a
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.1, delay: 1 }}
+        href="/images/CV Aurélia Heymann DeveloppeurFRONT.pdf"
+        download
+        className="px-4 py-2 bg-cyan-500 text-white font-bold rounded hover:bg-cyan-600 transition-colors"
+      >
+        Download CV
+      </motion.a>
 
 
-{/* 
+      {/* 
     <Document file="/images/cv.jpg">
       <Page pageNumber={1} />
     </Document>
@@ -101,24 +71,25 @@ export default function About() {
     <Viewer
       fileUrl="/images/cv.jpg"
       plugins={[defaultLayoutPluginInstance]}
-    />  */}  
-   
-   
-    <motion.img
-      initial={{ y: -50, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 1, delay: 1 }}
+    />  */}
 
-      // initial={{ opacity: 0 }}
-      // //animate={{ opacity: 1 }}
-      // whileInView={{ opacity: 1 }}
-      // transition={{ duration: 2 }}
 
-      src="/images/cv.png"
-      alt="CV Aurélia Heymann"
-      // className="hidden rounded-lg shadow-lg w-full h-full md:block"
-      className="rounded-lg shadow-xl border-4 border-white border-opacity-60 w-full h-full md:block "
-    />   
-    
-  </div>
+      <motion.img
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1, delay: 1 }}
+
+        // initial={{ opacity: 0 }}
+        // //animate={{ opacity: 1 }}
+        // whileInView={{ opacity: 1 }}
+        // transition={{ duration: 2 }}
+
+        src="/images/cv.png"
+        alt="CV Aurélia Heymann"
+        // className="hidden rounded-lg shadow-lg w-full h-full md:block"
+        className="rounded-lg shadow-xl border-4 border-white border-opacity-60 w-full h-full md:block "
+      />
+
+    </div>
+  )
 }
